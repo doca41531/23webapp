4245fabddf527c975f515722a12f2885a84cde62
doca41531 <doca41531@gmail.com>
2023-12-07 22:57:42 +0900

    임시 와

--- script.js
+++ script.js
@@ -11,10 +11,14 @@ app.set("views", "./views")
 app.set("view engine", "ejs")
 
 app.get("/", (req,res) => {
-    res.render("home/index")
+    res.render("home/main")
+})
+app.get("/seoul", (req,res) => {
+    res.render("home/seoul")
 })
 
 // import home from "./public/js/home"
+
 app.use(express.static(path.join(__dirname, 'static')));
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(express.static(path.join(__dirname, 'views')));
